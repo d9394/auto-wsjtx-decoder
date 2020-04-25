69a268dbed0bae3d2cc3b96e1d9e1c3419acc87a
d9394 <9394952@gmail.com>
2020-04-25 11:45:44 +0800

    Update ft8-decoder.py

--- ft8-decoder.py
+++ ft8-decoder.py
@@ -12,9 +12,10 @@ import uuid
 import wave
 #import os
 import subprocess
-from socket import * 
-
 #from basicft8 import *
+from socket import * 
+import pskreport
+import re
 
 def decoder(wav_file) :
 #	print("WAV file %s" % wav_file)
@@ -22,32 +23,76 @@ def decoder(wav_file) :
 	r = FT8()
 	p = r.gowav(wav_file, 0)
 	"""
-	p = subprocess.check_output(["jt9","-8","-d","3",wav_file])
+	p = subprocess.check_output(["jt9","-8","-d","3","-t","/dev/shm/","-a","/dev/shm/",wav_file])
 
 #	print("%s" % p.decode('utf-8').split("\n"))
 	if p != 0 :
-		nmSocket = socket(AF_INET,SOCK_DGRAM)
-		ip_port = (gethostbyname('192.168.103.1'), 5556)
-		nmSocket.sendto('Start Decode FT8-Raspberry-159'.encode('utf-8'),ip_port)
-		iq = open('/dev/shm/ft8.txt', "a")
-		for line in p.decode("utf-8").split("\n") :
-			try:
-				detail=line.split(' ')
-				a = int(detail[0])
-				adif = ''
-				print("+++++ %s" % line)
-				iq.write(line + '\n')
-				nmSocket.sendto(line.encode('utf-8'),ip_port)
-			except :
-				print("----- %s" % line)
-		iq.close()
-		nmSocket.sendto('End Decode FT8-Raspberry-159'.encode('utf-8'),ip_port)
-		nmSocket.close()
+		udp_send(p)
+		psk_send(p)
 		subprocess.call(['rm','-f',wav_file])
 #	os.remove(wav_file)
 
+def udp_send(result) :
+	global nmSocket, ip_port
+	nmSocket.sendto('Start Decode 14074000-FT8-Raspberry159'.encode('utf-8'),ip_port)   #send Identification of this decoder
+	iq = open('/dev/shm/ft8.txt', "a")            #log write to a file for debug or other program use.
+	for line in result.decode("utf-8").split("\n") :
+		try:
+			detail=line.split()
+			a = int(detail[0])
+			adif = ''
+			print("+++++ %s" % line)
+			iq.write(line + '\n')
+			nmSocket.sendto(line.encode('utf-8'),ip_port)      #send to my udp2telnet server
+		except :
+			a = 0      #nothing
+#			print("----- %s" % line)
+	iq.close()
+	nmSocket.sendto('End Decode FT8-Raspberry159'.encode('utf-8'),ip_port)     #nothing only for Identification of this decoder
+
+def iscall(call):
+	if len(call) < 3:
+		return False
+	if re.search(r'[0-9][A-Z]', call) == None:
+		# no digit
+		return False
+	#if self.sender.packcall(call) == -1:
+		# don't know how to encode this call, e.g. R870T
+	#	return False
+	return True
+		
+def psk_send(result) :
+	global pskr
+	for line in result.decode("utf-8").split("\n") :
+		detail=line.split()
+		#try:
+		if len(detail) > 5 :
+#			print("line %s" % detail)
+			txt = line[24:60]
+			hz = int(line[16:20].strip()) + int(14074000.0)
+			tm = int(datetime.datetime.now().strftime('%s'))
+
+			txt = txt.strip()
+			txt = re.sub(r'  *', ' ', txt)
+			txt = re.sub(r'CQ DX ', 'CQ ', txt)
+			txt = re.sub(r'CQDX ', 'CQ ', txt)
+			mm = re.search(r'^CQ ([0-9A-Z/]+) ([A-R][A-R][0-9][0-9])$', txt)
+
+			if mm != None and iscall(mm.group(1)):
+				if pskr != None:
+#					print("pskr {}, {}, {}, {}, {}\n".format(mm.group(1), hz, "FT8", mm.group(2), tm))
+					pskr.got(mm.group(1), hz, "FT8", mm.group(2), tm)
+			mm = re.search(r'^([0-9A-Z/]+) ([0-9A-Z/]+) ([A-R][A-R][0-9][0-9])$', txt)
+			if mm != None and iscall(mm.group(1)) and iscall(mm.group(2)):
+				if pskr != None:
+#					print("pskr {} {}, {}, {}, {}, {}\n".format(mm.group(1),mm.group(2), hz, "FT8", mm.group(3), tm))
+					pskr.got(mm.group(2), hz, "FT8", mm.group(3), tm)
+		#except Exception as e:
+		#	print("psk send error %s" % e)
+	
 	
 def main():
+	global nmSocket, ip_port, pskr
 	verbose = False
 	nametag = ''
 	tmp_path = '/dev/shm'
@@ -66,7 +111,6 @@ def main():
 	samplerate = 12000
 	chunksize = 32768
 
-
 	block = bytearray(0)
 	time1 = int(int(datetime.datetime.now().strftime('%s')) / blocktime) * blocktime
 	starttime = datetime.datetime.fromtimestamp(time1)
@@ -75,6 +119,11 @@ def main():
 
 	first=True
 
+	nmSocket = socket(AF_INET,SOCK_DGRAM)
+	ip_port = (gethostbyname('192.168.1.1'), 5556)    #send udp to my udp2telnet server
+	
+	pskr = pskreport.T("callsign", "aa11", "ft8-decoder", testing=False)    #send udp to pskreporter.info
+	
 	while True:
 		data = sys.stdin.buffer.read(chunksize)
 		block.extend(data)
@@ -106,6 +155,7 @@ def main():
 			starttime = datetime.datetime.fromtimestamp(time1)
 			endtime = datetime.datetime.fromtimestamp(time1+15)
 			block = bytearray(0)
+	nmSocket.close()
 
 if __name__ == '__main__':
 	main()
