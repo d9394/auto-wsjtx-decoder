e811551409a16b3bac193526e573344803e5c2a6
d9394 <9394952@gmail.com>
2020-04-25 21:22:04 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,8 +8,7 @@ do
 	}
 		
 	trap _term SIGTERM SIGINT
-
-	#netcat -l -p 1234 | python3 splitter.py &
+	
 	aa=$(netstat -nt | grep 5678 | wc -l)
 	if [ $aa -eq 0 ]; then
 		rtl_fm -f 64074000 -M usb - | sox -t raw -b 16 -c 1 -r 24k -es -V1 - -t wav -r 12k - | nc -k -l -p 5678 &
